99eafa13ec0c393c04dedac7fff5ab4bfd00def3
minji <jhchoi0226@snu.ac.kr>
2025-01-30 22:14:22 +0900

    following follwer provider

--- frontend/lib/providers/user_provider.dart
+++ frontend/lib/providers/user_provider.dart
@@ -5,9 +5,13 @@ import '../api/auth_api.dart';
 class UserProvider with ChangeNotifier {
   User? _user;
   bool _isLoading = false;
+  int _followersCount = 0;
+  int _followingCount = 0;
 
   User? get user => _user;
   bool get isLoading => _isLoading;
+  int get followersCount => _followersCount;
+  int get followingCount => _followingCount;
 
   Future<void> fetchUserProfile() async {
     if (_user != null) return; // 이미 데이터가 있으면 다시 요청하지 않음
@@ -25,6 +29,17 @@ class UserProvider with ChangeNotifier {
       _isLoading = false;
       notifyListeners();
       rethrow;
+    }
+  }
+
+  Future<void> fetchProfileStats() async {
+    try {
+      final response = await AuthAPI().getProfileStats();
+      _followersCount = response['followers_count'];
+      _followingCount = response['following_count'];
+      notifyListeners();
+    } catch (e) {
+      print('Failed to fetch profile stats: $e');
     }
   }
 }
